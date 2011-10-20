071ae2781e017fd0dce70aaa6735197c4ef30b71
Eric Stratmann <estrat@cs.stanford.edu>
2011-10-20 16:24:44 -0700

    Added type signatures

--- estrat-hilbert/HilbertTreeTest.hs
+++ estrat-hilbert/HilbertTreeTest.hs
@@ -1,10 +1,10 @@
 import Test.QuickCheck
-import Control.Monad
 import HilbertTree
 import Data.List
 import Data.Ord
 
 
+main :: IO ()
 main = do
     q "Don't find bogus rectangle" p1
     q "Search tree" p2
@@ -14,6 +14,7 @@ main = do
     q "Find dups" p6
     q "MBR matches all in tree" p7
 
+q :: Testable p => String -> p -> IO ()
 q s p = putStrLn s >> quickCheck p
 
 instance Arbitrary Rectangle where
@@ -35,31 +36,36 @@ instance Arbitrary HilbertTree where
         rects <- listOf arbitrary
         return $ buildTree rects
 
+p1 :: HilbertTree -> Bool
 p1 tree = 0 == length (searchTree tree (Rectangle (-2) (-1) (-2) (-1)))
 
+p2 :: HilbertTree -> Rectangle -> Bool
 p2 tree rect = rect `elem` searchTree (insertTree tree rect) rect
 
+p3 :: HilbertTree -> Bool
 p3 tree = allRects tree == sortBy (comparing hilbert) (allRects tree)
 
+p4 :: HilbertTree -> Bool
 p4 tree = 1 >= length (group $ mapDepth 0 tree)
 
+p5 :: HilbertTree -> Bool
 p5 = checkSize
 
+p6 :: HilbertTree -> Rectangle -> Bool
 p6 tree rect = 2 <= length (searchTree newTree rect)
     where newTree = insertTree (insertTree tree rect) rect
 
+p7 :: HilbertTree -> [Rectangle] -> Bool
 p7 tree rects = and $ map (`elem` results) rects
     where
     results = searchTree newTree mbr
     mbr = foldl1' calculateMBR rects
     newTree = foldl' insertTree tree rects
 
+checkSize :: HilbertTree -> Bool
 checkSize (Node ns) = length ns <= maxNodeSize && all (\(_,c,_) -> checkSize c) ns
 checkSize (Leaf rs) = length rs <= maxLeafSize 
 
 mapDepth :: Int -> HilbertTree -> [Int]
 mapDepth d (Node ns) = (\(_,c,_) -> mapDepth (d+1) c) =<< ns
 mapDepth d (Leaf _) = [d]
-
-
-getNodes (Node ns) = ns
